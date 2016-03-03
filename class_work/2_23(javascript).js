9ecec7b68d2b587d266e9de2a266f1940221f443
Ashley Asquith <asquithashley@gmail.com>
2016-03-03 00:14:44 -0500

    trying to sheet layout

---
+++ class_work/2_23(javascript).js
@@ -0,0 +1,21 @@
+function sayHi(name) {
+	return alert("hi " + name) ; 
+}
+
+sayHi("Shirmung");
+sayHi("jill");
+
+
+function saySomething(ashcash) {
+	return console.log("banana apple");
+}
+
+saySomething
+saySomething
+
+function openDoor(doorNumber) {
+	if (doorNumber ===1) {
+		console.log("you win a banana")
+	} else if (doorNumber ===2) {
+		console.log(" you win a apple")
+	}
